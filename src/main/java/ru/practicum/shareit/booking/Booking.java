bcbe11278d8ff023b4cb8761bc34108d99b28963
NikolayPatov <nikolay.patov@gmail.com>
2024-07-10 11:51:36 +0300

    feat: initial commit

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class Booking {
+    private Long id;
+    private LocalDate start;
+    private LocalDate end;
+    private boolean confirmed;
+    private String review;
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.request;
 
+import lombok.Data;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
 public class ItemRequest {
+    private Long id;
+    private String name;
+    private String description;
 }
