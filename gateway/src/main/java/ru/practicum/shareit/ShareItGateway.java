296c291a65072b1cab0048db374e3c2a3ea8ed11
NikolayPatov <nikolay.patov@gmail.com>
2024-08-29 08:21:11 +0300

    fix: fixing port exposure

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItGateway {
     public static void main(String[] args) {
-        System.setProperty("spring.config.location", "file:/path/to/your/application.properties");
         SpringApplication.run(ShareItGateway.class, args);
     }
 
